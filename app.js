68769f83769ed1db516787d66141c371f71f92c7
MityaDSCH <mlsgearon@gmail.com>
2015-06-09 19:26:08 -0500

    Redraws logo, cross-browser and visual optimizations for the nav-bar

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 var main = function() {
     
-    
+    $(window).scroll(function() {
+        
+        var wScroll = $(this).scrollTop();
+        
+        if (wScroll > $('#home-section').offset().top) {
+            $("nav").removeClass("hidden");
+        }
+        else {
+            $("nav").addClass("hidden");
+        }
+        
+    });
     
 };
 
