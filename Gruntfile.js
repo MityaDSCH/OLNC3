fe70b28e7e3dcd2c4bc420827f9f6f66858edcf7
MityaDSCH <mlsgearon@gmail.com>
2015-11-11 17:54:41 -0600

    gruntfile up and running

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,20 +8,30 @@ module.exports = function(grunt) {
     banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
       '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
       '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
-      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+      '* <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>; */\n',
     // Task configuration.
     jshint: {
       files: ['app.js']
     },
 
-    clean: ['./temp', './dist'],
+    clean: ['./temp/', './dist/'],
+
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      target: {
+        files: {
+          'dist/app.min.js': ['app.js']
+        }
+      }
+    },
 
     compass: {
       dist: {
         options: {
           sassDir: 'sass',
-          cssDir: 'stylesheets',
+          cssDir: 'temp',
           environment: 'production'
         }
       },
@@ -34,13 +44,19 @@ module.exports = function(grunt) {
       }
     },
 
+    cssmin: {
+      target: {
+        files: {
+          'dist/stylesheets/screen.css': ['temp/screen.css','bower_components/animate.css/animate.min.css']
+        }
+      }
+    },
+
     copy: {
       target: {
         files: [
-          {expand: true, src: ['stylesheets/**', 'images/**'], dest: 'dist/'},
-          {expand: true, src: ['index.html'], dest: 'dist/'},
-          {expand: true, src: ['app.min.js', 'scrollTo.js'], dest: 'dist/'},
-          {expand: true, src: ['animate.css'], dest: 'dist/'}
+          {expand: true, cwd: './', src: ['stylesheets/**', 'images/**'], dest: 'dist/'},
+          {expand: true, cwd: './', src: ['index.html'], dest: './dist/'}
         ]
       }
     },
@@ -49,17 +65,25 @@ module.exports = function(grunt) {
       serve: {
         options: {
           port: 9000,
-          base: './',
-          keepalive: true
+          base: './'
         }
       },
       build: {
         options: {
           port: 9000,
-          base: './dist',
-          keepalive: true
+          base: './dist/'
         }
       }
+    },
+
+    watch: {
+      css: {
+        files: ['sass/screen.scss'],
+        tasks: ['compass:dev'],
+        options: {
+          livereload: true
+        }
+      } 
     }
 
   });
@@ -68,17 +92,18 @@ module.exports = function(grunt) {
   [
     'grunt-contrib-clean',
     'grunt-contrib-uglify',
-    'grunt-contrib-concat',
     'grunt-contrib-jshint',
     'grunt-contrib-compass',
     'grunt-contrib-connect',
-    'grunt-contrib-copy'
+    'grunt-contrib-copy',
+    'grunt-contrib-cssmin',
+    'grunt-contrib-watch'
   ].forEach(function(ele) {
     grunt.loadNpmTasks(ele);
   });
 
   // tasks.
-  grunt.registerTask('default', ['jshint', 'compass:dev', 'connect:serve']);
-  grunt.registerTask('build', ['jshint', 'clean', 'compass:dist', 'copy', 'connect:build']);
+  grunt.registerTask('default', ['jshint', 'compass:dev', 'connect:serve', 'watch']);
+  grunt.registerTask('build', ['jshint', 'clean', 'uglify', 'compass:dist', 'cssmin', 'copy', 'connect:build']);
 
 };
